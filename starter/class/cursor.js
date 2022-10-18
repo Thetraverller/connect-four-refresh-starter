30c5185707ee27ed357682d2c472d5984ca92bda
Tanzil <mtanzilislam1997@gmail.com>
2022-10-18 12:27:40 +0100

    help required

--- starter/class/cursor.js
+++ starter/class/cursor.js
@@ -20,14 +20,47 @@ class Cursor {
 
   setBackgroundColor() {
     Screen.setBackgroundColor(this.row, this.col, this.cursorColor);
+    Screen.render();
   }
 
   left() {
+    this.resetBackgroundColor();
     // Move cursor left
+    if (this.col > 0) {
+      this.col -= 1;
+    }
+
+    this.setBackgroundColor();
   }
 
   right() {
+    this.resetBackgroundColor();
     // Move cursor right
+    if (this.col < this.numCols - 1) {
+      this.col += 1;
+    }
+
+    this.setBackgroundColor();
+  }
+
+  up() {
+    this.resetBackgroundColor();
+    // Move cursor left
+    if (this.row > 0) {
+      this.row -= 1;
+    }
+
+    this.setBackgroundColor();
+  }
+
+  down() {
+    this.resetBackgroundColor();
+    // Move cursor right
+    if (this.row < this.numRows - 1) {
+      this.row += 1;
+    }
+
+    this.setBackgroundColor();
   }
 
 }
